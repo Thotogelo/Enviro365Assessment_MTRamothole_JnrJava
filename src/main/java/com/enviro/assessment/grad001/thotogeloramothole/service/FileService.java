6f22f79c09bf11673e245fcc28854565640efa9b
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-13 18:40:30 +0200

    Implemented file service

--- src/main/java/com/enviro/assessment/grad001/thotogeloramothole/service/FileService.java
+++ src/main/java/com/enviro/assessment/grad001/thotogeloramothole/service/FileService.java
@@ -1,7 +1,39 @@
 package com.enviro.assessment.grad001.thotogeloramothole.service;
 
+import com.enviro.assessment.grad001.thotogeloramothole.Exception.FileProcessingException;
+import com.enviro.assessment.grad001.thotogeloramothole.Exception.FileStorageException;
+import com.enviro.assessment.grad001.thotogeloramothole.Models.File;
+import com.enviro.assessment.grad001.thotogeloramothole.Models.ProcessedData;
+import com.enviro.assessment.grad001.thotogeloramothole.repository.FileRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Service
 public class FileService {
+    private final FileRepository fileRepository;
+
+    public FileService(FileRepository fileRepository) {
+        this.fileRepository = fileRepository;
+    }
+
+    public void storeFile(MultipartFile file) {
+        try {
+            if (file.isEmpty()) {
+                throw new FileStorageException("Failed to store empty file." + file.getOriginalFilename());
+            }
+
+            File fileToStore = new File();
+            ProcessedData processedData = new ProcessedData();
+
+            fileToStore.setFileName(file.getOriginalFilename());
+            processedData.setFileId(fileToStore.getId());
+            processedData.setProcessedData(file.getBytes());
+            fileRepository.save(fileToStore);
+        } catch (IOException e) {
+            throw new FileProcessingException("Failed to process file." + file.getOriginalFilename(), e);
+        }
+    }
+
 }
