f0e7c504706d7064d36fa6250f045694bb8478f4
Thotogelo <ramotholethotogelo@gmail.com>
2024-04-13 12:25:06 +0200

    initial commit

---
+++ src/test/java/com/enviro/assessment/grad001/thotogeloramothole/EnviroAssessmentGradMtRamotholeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.thotogeloramothole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnviroAssessmentGradMtRamotholeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
