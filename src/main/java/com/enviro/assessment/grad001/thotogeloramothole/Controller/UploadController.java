4c3b4ef733cf27239804879adf86b7288a2a403c
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-13 18:15:51 +0200

    Implementing controller class

--- src/main/java/com/enviro/assessment/grad001/thotogeloramothole/Controller/UploadController.java
+++ src/main/java/com/enviro/assessment/grad001/thotogeloramothole/Controller/UploadController.java
@@ -1,22 +1,36 @@
 package com.enviro.assessment.grad001.thotogeloramothole.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.enviro.assessment.grad001.thotogeloramothole.service.FileService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/v1/api/upload")
+@RequestMapping("/v1/api/file")
 public class UploadController {
+
+    private final FileService fileService;
+
+    public UploadController(FileService fileService) {
+        this.fileService = fileService;
+    }
 
     @GetMapping
     public String test() {
         return "Upload controller working";
     }
 
-    @PostMapping
-    public String fileUpload() {
-        return "File uploaded";
-    }
+    @PostMapping("/upload")
+    public ResponseEntity<String> fileUpload(@RequestParam("file") MultipartFile file) {
+        try {
+            if (file.isEmpty()) {
+                return ResponseEntity.badRequest().body("File is empty.");
+            }
+            fileService.storeFile(file);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Failed to upload file.");
+        }
 
+        return ResponseEntity.ok("File uploaded successfully");
+    }
 }
