fe46859c5c62756b7f333ad10b76634707ff54c5
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-13 18:45:37 +0200

    Add API logger

---
+++ src/main/java/com/enviro/assessment/grad001/thotogeloramothole/Logger/LoggingInterceptor.java
@@ -0,0 +1,8 @@
+package com.enviro.assessment.grad001.thotogeloramothole.Logger;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class LoggingInterceptor implements HandlerInterceptor {
+}
