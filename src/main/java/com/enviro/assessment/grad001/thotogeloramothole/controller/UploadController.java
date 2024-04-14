d3af0a6da1229e16a1d507bb3e8ac589989c101b
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-14 15:20:19 +0200

    Improve error handling and logging

--- src/main/java/com/enviro/assessment/grad001/thotogeloramothole/controller/UploadController.java
+++ src/main/java/com/enviro/assessment/grad001/thotogeloramothole/controller/UploadController.java
@@ -1,7 +1,11 @@
 package com.enviro.assessment.grad001.thotogeloramothole.controller;
 
+import com.enviro.assessment.grad001.thotogeloramothole.exception.FileProcessingException;
+import com.enviro.assessment.grad001.thotogeloramothole.exception.FileStorageException;
 import com.enviro.assessment.grad001.thotogeloramothole.model.File;
 import com.enviro.assessment.grad001.thotogeloramothole.service.FileService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,11 +15,14 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 import java.util.Objects;
 
+
 @RestController
 @RequestMapping("/v1/api/file")
 public class UploadController {
 
     private final FileService fileService;
+    private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
+
 
     public UploadController(FileService fileService) {
         this.fileService = fileService;
@@ -23,8 +30,13 @@ public class UploadController {
 
 
     @GetMapping("/{fileid}")
-    public ResponseEntity<File> getProcessedData(@PathVariable("fileid") Long fileid) {
-        return ResponseEntity.ok(fileService.getFileById(fileid));
+    public ResponseEntity<?> getProcessedData(@PathVariable("fileid") Long fileid) {
+        try {
+            return ResponseEntity.ok(fileService.getFileById(fileid));
+        } catch (FileProcessingException e) {
+            logger.error("Error getting processed data by id: " + fileid, e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while retrieving the file: " + e.getMessage());
+        }
     }
 
     @GetMapping("/data")
@@ -32,8 +44,8 @@ public class UploadController {
         try {
             List<File> files = fileService.getAllFiles();
             return ResponseEntity.ok(files);
-        } catch (Exception e) {
-            // Return an error response to the client
+        } catch (FileProcessingException e) {
+            logger.error("An error occurred while retrieving the data: ", e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while retrieving the data: " + e.getMessage());
         }
     }
@@ -42,19 +54,31 @@ public class UploadController {
     public ResponseEntity<String> fileUpload(@RequestParam("file") MultipartFile file) {
 
         try {
+//            File empty validation
             if (file.isEmpty()) {
+                logger.error("File is empty.");
                 return ResponseEntity.badRequest().body("File is empty, please upload a text file with contents");
             }
 
+            // File size validation
+//            500 * 1024 = 512000; // 500KB
+            if (file.getSize() > 512000) {
+                logger.error("Provided a file larger than 500kb.");
+                return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).body("File is too large, please upload a file smaller than 500kb.");
+            }
+
+//            File type validation
             if (!MediaType.TEXT_PLAIN.equals(MediaType.parseMediaType(Objects.requireNonNull(file.getContentType())))) {
-                return ResponseEntity.badRequest().body("Please upload a text file.");
+                logger.error("Provided a non-text file.");
+                return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body("Please upload a text file.");
             }
 
             fileService.storeFile(file);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body("Failed to upload file." + e.getMessage());
+            return ResponseEntity.ok("File uploaded successfully.");
+        } catch (FileStorageException e) {
+            logger.error("Failed to upload file.", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload file." + e.getMessage());
         }
 
-        return ResponseEntity.ok("File uploaded successfully.");
     }
 }
