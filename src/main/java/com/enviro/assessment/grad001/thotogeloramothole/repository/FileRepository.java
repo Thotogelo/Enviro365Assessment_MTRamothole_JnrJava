3092e3f8964a38113f9feef69bcf8e8a01c1a810
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-14 10:57:15 +0200

    optimise imports

--- src/main/java/com/enviro/assessment/grad001/thotogeloramothole/repository/FileRepository.java
+++ src/main/java/com/enviro/assessment/grad001/thotogeloramothole/repository/FileRepository.java
@@ -1,7 +1,6 @@
 package com.enviro.assessment.grad001.thotogeloramothole.repository;
 
 import com.enviro.assessment.grad001.thotogeloramothole.model.File;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
