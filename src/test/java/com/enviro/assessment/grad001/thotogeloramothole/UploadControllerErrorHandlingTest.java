d47e6c5bc8ba5269aa791f4409723dfed7ef97ea
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-14 21:01:30 +0200

    Test controller exceptions only

--- src/test/java/com/enviro/assessment/grad001/thotogeloramothole/UploadControllerErrorHandlingTest.java
+++ src/test/java/com/enviro/assessment/grad001/thotogeloramothole/UploadControllerErrorHandlingTest.java
@@ -1,6 +1,7 @@
 package com.enviro.assessment.grad001.thotogeloramothole;
 
-import com.enviro.assessment.grad001.thotogeloramothole.service.FileService;
+import com.enviro.assessment.grad001.thotogeloramothole.exception.FileStorageException;
+import com.enviro.assessment.grad001.thotogeloramothole.service.IFileService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -9,8 +10,10 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -20,34 +23,46 @@ class UploadControllerErrorHandlingTest {
     private MockMvc mockMvc;
 
     @MockBean
-    private FileService fileService;
+    private IFileService fileService;
 
     @Test
     void testEmptyFileUpload() throws Exception {
         MockMultipartFile emptyFile = new MockMultipartFile("file", "", "text/plain", "".getBytes());
+        String message = "File is empty, please upload a text file with contents";
+
+        doThrow(new FileStorageException(message))
+                .when(fileService).storeFile(emptyFile);
 
         mockMvc.perform(multipart("/v1/api/file/upload").file(emptyFile))
                 .andExpect(status().isBadRequest())
-                .andExpect(content().string("File is empty, please upload a text file with contents"));
+                .andExpect(content().string(message));
     }
 
     @Test
     void testLargeFileUpload() throws Exception {
         byte[] largeFileContent = new byte[512001]; // size > 500KB
         MockMultipartFile largeFile = new MockMultipartFile("file", "test.txt", "text/plain", largeFileContent);
+        String message = "File is too large, please upload a file smaller than 500kb.";
+
+        doThrow(new FileStorageException(message))
+                .when(fileService).storeFile(largeFile);
 
         mockMvc.perform(multipart("/v1/api/file/upload").file(largeFile))
                 .andExpect(status().isPayloadTooLarge())
-                .andExpect(content().string("File is too large, please upload a file smaller than 500kb."));
+                .andExpect(content().string(message));
     }
 
     @Test
     void testNonTextFileUpload() throws Exception {
         MockMultipartFile nonTextFile = new MockMultipartFile("file", "test.jpg", "image/jpeg", "test data".getBytes());
+        String message = "Please upload a text file.";
+
+        doThrow(new FileStorageException(message))
+                .when(fileService).storeFile(nonTextFile);
 
         mockMvc.perform(multipart("/v1/api/file/upload").file(nonTextFile))
                 .andExpect(status().isUnsupportedMediaType())
-                .andExpect(content().string("Please upload a text file."));
+                .andExpect(content().string(message));
     }
 
 }
