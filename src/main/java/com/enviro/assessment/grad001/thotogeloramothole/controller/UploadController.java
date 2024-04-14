e802bef1cf79e9f43f685c7ca4d891416832e086
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-14 18:48:14 +0200

    Remove unnecessary exception handlers

--- src/main/java/com/enviro/assessment/grad001/thotogeloramothole/controller/UploadController.java
+++ src/main/java/com/enviro/assessment/grad001/thotogeloramothole/controller/UploadController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -25,66 +26,49 @@ public class UploadController {
 
     private final FileService fileService;
     private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
-
+    private static final long MAX_FILE_SIZE = 512000; // 500KB
 
     public UploadController(FileService fileService) {
         this.fileService = fileService;
     }
 
 
-    @GetMapping("/{fileid}")
+    @GetMapping(value = "/{fileid}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "Get an environment data file by id", description = "Returns an environment data file by id")
-    public ResponseEntity<?> getProcessedData(@PathVariable("fileid") Long fileid) {
-        try {
-            return ResponseEntity.ok(fileService.getFileById(fileid));
-        } catch (FileProcessingException e) {
-            logger.error("Error getting processed data by id: {}", fileid, e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while retrieving the file: " + e.getMessage());
-        }
+    public ResponseEntity<File> getFilesById(@PathVariable("fileid") Long fileid) {
+        return ResponseEntity.ok(fileService.getFileById(fileid));
     }
 
-    @GetMapping("/data")
+    @GetMapping(value = "/data", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "Get all environment data file", description = "Returns all environment data files")
-    public ResponseEntity<?> getAllData() {
-        try {
-            List<File> files = fileService.getAllFiles();
-            return ResponseEntity.ok(files);
-        } catch (FileProcessingException e) {
-            logger.error("An error occurred while retrieving the data: ", e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while retrieving the data: " + e.getMessage());
-        }
+    public ResponseEntity<List<File>> getAllEnvironmentFiles() {
+        List<File> files = fileService.getAllFiles();
+        return ResponseEntity.ok(files);
     }
 
-    @PostMapping("/upload")
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "Upload an environment data file", description = "Upload an environment data file")
     public ResponseEntity<String> fileUpload(@RequestParam("file") MultipartFile file) {
-
-        try {
 //            File empty validation
-            if (file.isEmpty()) {
-                logger.error("File is empty.");
-                return ResponseEntity.badRequest().body("File is empty, please upload a text file with contents");
-            }
-
-            // File size validation
-//            500 * 1024 = 512000; // 500KB
-            if (file.getSize() > 512000) {
-                logger.error("Provided a file larger than 500kb.");
-                return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).body("File is too large, please upload a file smaller than 500kb.");
-            }
-
-//            File type validation
-            if (!MediaType.TEXT_PLAIN.equals(MediaType.parseMediaType(Objects.requireNonNull(file.getContentType())))) {
-                logger.error("Provided a non-text file.");
-                return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body("Please upload a text file.");
-            }
-
-            fileService.storeFile(file);
-            return ResponseEntity.ok("File uploaded successfully.");
-        } catch (FileStorageException e) {
-            logger.error("Failed to upload file.", e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload file." + e.getMessage());
+        if (file.isEmpty()) {
+            logger.error("File is empty.");
+            return ResponseEntity.badRequest().body("File is empty, please upload a text file with contents");
         }
 
+        // File size validation
+//            500 * 1024 = 512000; // 500KB
+        if (file.getSize() > MAX_FILE_SIZE) {
+            logger.error("Provided a file larger than 500kb.");
+            return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).body("File is too large, please upload a file smaller than 500kb.");
+        }
+
+//            File type validation
+        if (!MediaType.TEXT_PLAIN.equals(MediaType.parseMediaType(Objects.requireNonNull(file.getContentType())))) {
+            logger.error("Provided a non-text file.");
+            return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body("Please upload a text file.");
+        }
+
+        fileService.storeFile(file);
+        return ResponseEntity.ok("File uploaded successfully.");
     }
 }
