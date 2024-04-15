0f1bbd8a41aa554590cb350625569333151041b0
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-15 09:29:47 +0200

    Add comments

--- src/main/java/com/enviro/assessment/grad001/thotogeloramothole/exception/GlobalExceptionHandler.java
+++ src/main/java/com/enviro/assessment/grad001/thotogeloramothole/exception/GlobalExceptionHandler.java
@@ -8,14 +8,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    // Handles exceptions related to file processing
     @ExceptionHandler(FileProcessingException.class)
     public ResponseEntity<String> handleFileProcessingException(FileProcessingException e) {
+        // Returns a response with a status of 500 and a message describing the error
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred: " + e.getMessage());
     }
 
+    // Handles exceptions related to file storage
     @ExceptionHandler(FileStorageException.class)
     public ResponseEntity<String> handleFileStorageException(FileStorageException ex) {
-        // Determine the status code based on the exception message
+        // Determines the status code based on the exception message
         HttpStatus status = switch (ex.getMessage()) {
             case "File is empty, please upload a text file with contents" -> HttpStatus.BAD_REQUEST;
             case "File is too large, please upload a file smaller than 500kb." -> HttpStatus.PAYLOAD_TOO_LARGE;
@@ -23,6 +26,7 @@ public class GlobalExceptionHandler {
             default -> HttpStatus.INTERNAL_SERVER_ERROR;
         };
 
+        // Returns a response with the determined status code and the exception message
         return new ResponseEntity<>(ex.getMessage(), status);
     }
 }
