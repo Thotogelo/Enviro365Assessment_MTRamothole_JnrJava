25067b0fd171c4561a221ce4023486732f545302
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-18 07:31:59 +0200

    Use ProblemDetail in global exception handling

--- src/main/java/com/enviro/assessment/grad001/thotogeloramothole/exception/GlobalExceptionHandler.java
+++ src/main/java/com/enviro/assessment/grad001/thotogeloramothole/exception/GlobalExceptionHandler.java
@@ -1,7 +1,7 @@
 package com.enviro.assessment.grad001.thotogeloramothole.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -10,14 +10,14 @@ public class GlobalExceptionHandler {
 
     // Handles exceptions related to file processing
     @ExceptionHandler(FileProcessingException.class)
-    public ResponseEntity<String> handleFileProcessingException(FileProcessingException e) {
+    public ProblemDetail handleFileProcessingException(FileProcessingException e) {
         // Returns a response with a status of 500 and a message describing the error
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred: " + e.getMessage());
+        return ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, "An error occurred: " + e.getMessage());// ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred: " + e.getMessage());
     }
 
     // Handles exceptions related to file storage
     @ExceptionHandler(FileStorageException.class)
-    public ResponseEntity<String> handleFileStorageException(FileStorageException ex) {
+    public ProblemDetail handleFileStorageException(FileStorageException ex) {
         // Determines the status code based on the exception message
         HttpStatus status = switch (ex.getMessage()) {
             case "File is empty, please upload a text file with contents" -> HttpStatus.BAD_REQUEST;
@@ -27,6 +27,6 @@ public class GlobalExceptionHandler {
         };
 
         // Returns a response with the determined status code and the exception message
-        return new ResponseEntity<>(ex.getMessage(), status);
+        return ProblemDetail.forStatusAndDetail(status, ex.getMessage());
     }
 }
