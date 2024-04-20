5c91a92ec44ad9a63ae5c27125cb932ff2ee3e79
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-20 10:35:38 +0200

    Add logger

--- src/main/java/com/enviro/assessment/grad001/thotogeloramothole/exception/GlobalExceptionHandler.java
+++ src/main/java/com/enviro/assessment/grad001/thotogeloramothole/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.enviro.assessment.grad001.thotogeloramothole.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     // Handles exceptions related to file processing
     @ExceptionHandler(FileProcessingException.class)
     public ProblemDetail handleFileProcessingException(FileProcessingException e) {
+        logger.error("Error occurred", e);
         // Returns a response with a status of 500 and a message describing the error
         return ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR,
                 "An error occurred: " + e.getMessage());
@@ -27,6 +32,7 @@ public class GlobalExceptionHandler {
             default -> HttpStatus.INTERNAL_SERVER_ERROR;
         };
 
+        logger.error(ex.getMessage(), ex);
         // Returns a response with the determined status code and the exception message
         return ProblemDetail.forStatusAndDetail(status, ex.getMessage());
     }
